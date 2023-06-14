a0cb7eaec5c7bb7a2d92469d1f539fc2fdd7258b
yarikkelb <125504558+yarikkelb@users.noreply.github.com>
2023-06-14 22:26:00 +0300

    2225

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,9 @@
-const title = document.querySelector('.item');
+const item = document.querySelector('.item');
 console.log(title);
+//const itemEl = document.querySelector('.item');
+//console.log('Number of categories:', itemEl.length);
+
+//itemEl.forEach(function (item) {
+    //console.log('Category:', item.children[0].textContent, 'Elements:', item.children[1].children.length)
+
+//});
