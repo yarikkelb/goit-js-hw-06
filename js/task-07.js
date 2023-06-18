cbbf582d80a866decba5688b61b952623c875dde
yarikkelb <125504558+yarikkelb@users.noreply.github.com>
2023-06-18 18:51:31 +0300

    1851

---
+++ js/task-07.js
@@ -0,0 +1,7 @@
+document.querySelector('body').style.backgroundColor = '#eaf1fad0';
+const inputRangeRef = document.getElementById('font-size-control');
+const textRef = document.getElementById('text');
+const fontSizeControl = ({ currentTarget }) =>
+  (textRef.style.fontSize = `${currentTarget.value}px`);
+
+inputRangeRef.addEventListener('input', fontSizeControl);
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const textColor = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+const body = document.body;
+
+const changeColor = () => {
+  textColor.textContent = body.style.backgroundColor = getRandomHexColor();
+};
+changeColorBtn.addEventListener('click', changeColor);
