f60084b2e57523bd46edb09e1999eb3094c09232
yarikkelb <125504558+yarikkelb@users.noreply.github.com>
2023-06-18 21:50:51 +0300

    2150

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,15 @@
-document.querySelector('body').style.backgroundColor = '#EBECF0';
-const inputRef = document.getElementById('validation-input');
-inputRef.addEventListener('blur', onBlurBorderColor);
-function onBlurBorderColor(event) {
-  const inputDataLength = Number(inputRef.dataset.length);
-  const inputValueLength = Number(inputRef.value.trim().length);
+const input = document.querySelector("#validation-input");
+input.addEventListener("blur", handlerInputBlur)
 
-  if (inputValueLength === inputDataLength) {
-    inputRef.classList.add('valid');
-    inputRef.classList.remove('invalid');
-  } else {
-    inputRef.classList.remove('valid');
-    inputRef.classList.add('invalid');
-  }
+function handlerInputBlur() {
+  const dataLength = input.getAttribute("data-length");
+  console.log(dataLength);
+
+const currentLength = input.value.length;
+console.log(currentLength);
+input.classList.add("invalid");
+
+if (Number(dataLength) === Number(currentLength)) {
+  input.classList.replace("invalid", "valid");
 }
+}  
