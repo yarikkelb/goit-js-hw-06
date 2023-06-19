2de5ad4845410ba48124f5c8bfedde6806ff3fe7
yarikkelb <125504558+yarikkelb@users.noreply.github.com>
2023-06-19 18:40:11 +0300

    1840

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,10 @@
 
-const list = document.querySelectorAll('.item');
+const list = document.querySelectorAll(".item");
 console.log(list);
-console.log('Number of categories: ${list.length}');
+console.log(`Number of categories: ${list.length}`);
 
  list.forEach((li) => {
-  console.log('Category: ${li.firstElementChild.textConuent}');
-  console.log('Elements:${li.lastElementChild.children.length}');
+  console.log(`Category: ${li.firstElementChild.textContent}`);
+  console.log(`Elements: ${li.lastElementChild.children.length}`);
  });
 console.log(list.children);
